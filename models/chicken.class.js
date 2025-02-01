98d80df1cd5ccb1fb156ffe820780ee6f3ad3dd1
SnowCoder404 <lenschaumarco@protonmail.com>
2025-02-01 16:28:57 +0100

    Remove errors and deploy automatic of ftp server.

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -19,8 +19,16 @@ class Chicken extends MovableObject {
      * The vertical position of the chicken on the canvas.
      * @type {number}
      */
-    y = 380;
+    y = 370;
 
+    /**
+     * The offset values for collision detection.
+     * @type {Object}
+     * @property {number} top - The top offset for collision.
+     * @property {number} bottom - The bottom offset for collision.
+     * @property {number} left - The left offset for collision.
+     * @property {number} right - The right offset for collision.
+     */
     offset = {
         top: 20,
         bottom: 20,
@@ -31,6 +39,8 @@ class Chicken extends MovableObject {
     /**
      * Creates a new chicken instance with a random horizontal position and speed.
      * Initializes the chicken's images and starts movement and animation.
+     * 
+     * @param {Array<string>} chickenImagesVar - An array of image paths used for animating the chicken.
      */
     constructor(chickenImagesVar) {
         super().loadImage(chickenImagesVar[0]);
@@ -49,18 +59,24 @@ class Chicken extends MovableObject {
         this.loadImages(chickenImagesVar);
         this.move();
         this.showAnimation(chickenImagesVar);
-        
     }
 
+    /**
+     * Animates the chicken by cycling through its images.
+     * The animation is set to update at regular intervals.
+     * 
+     * @param {Array<string>} chickenImagesVar - An array of image paths for the chicken's animation.
+     */
     showAnimation(chickenImagesVar) {
-        // Animates the chicken by cycling through its images.
         setStoppableInterval(() => {
             this.playAnimation(chickenImagesVar);
         }, 200);
     }
 
+    /**
+     * Moves the chicken left continuously at a fixed speed.
+     */
     move() {
-        // Moves the chicken left continuously.
         setStoppableInterval(() => {
             this.moveLeft();
         }, 1000 / 60);
--- models/cloud.class.js
+++ models/cloud.class.js
@@ -1,8 +1,34 @@
-class Cloud extends MovableObject{
+/**
+ * Represents a cloud object that is movable and can be drawn on the canvas.
+ * The cloud object extends from the MovableObject class, inheriting movement and image loading functionality.
+ */
+class Cloud extends MovableObject {
+    /**
+     * The width of the cloud object.
+     * @type {number}
+     * @default 500
+     */
     width = 500;
+    
+    /**
+     * The height of the cloud object.
+     * @type {number}
+     * @default 150
+     */
     heigth = 150;
+
+    /**
+     * The y position of the cloud object on the canvas.
+     * @type {number}
+     * @default 0
+     */
     y = 0;
 
+    /**
+     * Creates an instance of the Cloud class, loading the cloud image and setting the initial position.
+     * The cloud will start at the x position of 480 and at the top of the canvas (y = 0).
+     * It calls the constructor of the MovableObject class to inherit its functionality.
+     */
     constructor() {
         super().loadImage(cloudImage);
         this.x = 480;

0fa3e4331981800c7a9298d03e605812bb2313f5
SnowCoder404 <lenschaumarco@protonmail.com>
2025-02-22 23:12:09 +0100

    Improve the eslint config. Add a rule for cammel case.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,31 +2,44 @@ import globals from "globals";
 import jsdoc from "eslint-plugin-jsdoc";
 
 export default [
-    {
-      files: ["scripts/*.js", "models/*.js"],
-      languageOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
-        globals: globals.browser
-      },
-      rules: {
-        "max-lines-per-function": ["warn", 13],
-        "semi": ["error", "always"],
-        "quotes": ["error", "double"],
-        "no-console": ["error"],
-        "indent": ["warn", 4],
-        "prefer-const": ["warn"],
-        "jsdoc/require-jsdoc": ["error", {
+  {
+    files: ["scripts/*.js", "models/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: globals.browser
+    },
+    rules: {
+      "max-lines-per-function": ["warn", 13],
+      "semi": ["error", "always"],
+      "quotes": ["error", "double"],
+      "no-console": ["error"],
+      "indent": ["warn", 4],
+      "prefer-const": ["warn"],
+      "jsdoc/require-jsdoc": [
+        "error",
+        {
           "require": {
             "FunctionDeclaration": true,
             "MethodDefinition": true,
             "ArrowFunctionExpression": true,
             "FunctionExpression": true
           }
-        }]
-      },
-      plugins: {
-        jsdoc: jsdoc
-      }
+        }
+      ],
+      "camelcase": [
+        "error",
+        {
+          "properties": "always", 
+          "ignoreDestructuring": false,
+          "ignoreImports": false,
+          "ignoreGlobals": false,
+          "allow": ["^([a-z0-9]+(_[a-z0-9]+)*_)?sound$", "^([a-z0-9]+(_[a-z0-9]+)*_)?x$", "^([a-z0-9]+(_[a-z0-9]+)*_)?y$"]
+        }
+      ]
+    },
+    plugins: {
+      jsdoc: jsdoc
     }
+  }
 ];
